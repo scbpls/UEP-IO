16b504581a0ad2ab7e7b73da057a31104457221c
adoyxx <adam.luczka99@gmail.com>
2025-03-10 10:28:14 +0100

    added newbie version of main()

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from openai import OpenAI
 
 def main():
+    strawberry_kcal = 0.33
+    daily_kcal = 2250
+    strawberries_needed = round(daily_kcal/strawberry_kcal,2)
+    print(f"Potrzebujesz {strawberries_needed} truskawek")
+    return strawberries_needed
+
+def lepszy_main():
     api_key = input("Podaj klucz OpenAI:\n")
     client = OpenAI(api_key=api_key)
     response = client.chat.completions.create(
