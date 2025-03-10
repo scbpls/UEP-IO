cc95c8edfedb549317044d3b2b9512ca22f4b85a
adoyxx <adam.luczka99@gmail.com>
2025-03-10 10:14:47 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+
+def main():
+    api_key = input("Podaj klucz OpenAI:\n")
+    client = OpenAI(api_key=api_key)
+    response = client.chat.completions.create(
+            model="gpt-4o-2024-08-06",
+            messages=[
+                {"role": "system", "content": "Zwracaj jedynie odpowiedź w formie liczby zmiennoprecinkowej"},
+                {"role": "user", "content": """
+                Zakładamy, że 100 gramów truskawki zawiera 33 kcal, przeciętne dzienne
+                zapotrzebowanie kcal dla człowieka, to 2250 kcal, a cała energia pochodzi 
+                wyłącznie z truskawek – nie ma innych źródeł kalorii. Zmiennoprzecinkowa 
+                liczba truskawek w gramach zaokragląna do dwóch miejsc po przecinku
+                """},
+            ], 
+            #UWAGA - o3 nie przyjmuje temperature i max_tokens
+            temperature=0.0,
+            max_tokens=10
+        )
+    openai_choice = response.choices[0].message.content
+    print(f"Potrzebujesz {openai_choice} truskawek")
+    return openai_choice
+
+main()
